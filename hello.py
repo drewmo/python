d361af7b5169195758e975f57daec6cc5278dd7d
drewmo <darkdrewmo@gmail.com>
2014-10-16 12:11:49 -0700

    added guessing game

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 # This programs says hello and asks for my name.
+
 print('Hello world!')
 print('What is your name?')
 myName = input()
-print('It is good to meey you ' + myName)
+print('It is good to meet you, ' + myName)
 

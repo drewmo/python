533fdbce1cb996cb205da9fbb7df504114ad414a
drewmo <darkdrewmo@gmail.com>
2014-10-16 17:02:50 -0700

    added randomized text

--- dragonRealm/dragon.py
+++ dragonRealm/dragon.py
@@ -34,47 +34,107 @@ def chooseCaveDescript():
 	return str(theCave[caveD])
 
 def chooseDragonDescript():
-	dragonD = random.randint(1,5)
-	dragonD1 = random.randint(1,5)
-	dragonSize = {
+	dragonD = random.randint(1,15)
+	dragonD1 = random.randint(1,15)
+	dragonAdj = {
 		1:' large',
 		2:' small',
 		3:'n average',
 		4:' baby',
 		5:' tiny'
+		6:' huge',
+		7:' volcanic',
+		8:' spectral',
+		9:' contagious',
+		10:' steaming',
+		11:' frozen',
+		12:' dripping',
+		13:' shiny',
+		14:' hiding',
+		15:' venomous'
 	}
-	dragonAdj = {
+	dragonAdj1 = {
 		1:'angry',
 		2:'nervous',
 		3:'skeletal',
 		4:'horned',
-		5:'racist'	
+		5:'racist',
+		6:'goth-like',
+		7:'blue',
+		8:'red',
+		9:'fat',
+		10:'rotten',
+		11:'velvet',
+		12:'calculating',
+		13:'horrifying',
+		14:'fantastic',
+		15:'confused',	
 	}
-	return str(dragonSize[dragonD] + ' ' + dragonAdj[dragonD1])	
+	return str(dragonAdj[dragonD] + ' ' + dragonAdj1[dragonD1])	
 
 def chooseLootDescript():
-	lootD = random.randint(1,5)
-	lootD1 = random.randint(1,3)
+	lootD = random.randint(1,11)
+	lootD1 = random.randint(1,23)
+	lootD2 = random.randint(1,18)
 	lootAdj = {
 		1:'shiny',
 		2:'scuffed',
 		3:'broken',
 		4:'confusing',
-		5:'useful'	
+		5:'useful',
+		6:'broken',
+		7:'orange',
+		8:'curly',
+		9:'shattered',
+		10:'vibrating',
+		11:'secret'	
 	}	
 	lootName = {
 		1:'collectable card',
 		2:'golden cup',
 		3:'sword',
 		4:'stuff',
-		5:'golden plates'	
+		5:'golden plates',
+		6:'Orin Berry',
+		7:'shield',
+		8:'boot',
+		9:'silverware',
+		10:'chalice',
+		11:'frog',
+		12:'princess',
+		13:'glasses',
+		14:'chest',
+		15:'axe',
+		16:'whip',
+		17:'scale',
+		18:'bat wing',
+		19:'skeleton bone',
+		20:'horse hooves',
+		21:'battle armor',
+		22:'mithril thong',
+		23:'silver grinder'
 	}
 	lootType = {
 		1:'collection',
 		2:'stash',
-		3:'pile'	
+		3:'pile',
+		4:'',
+		5:'box',
+		6:'picture',
+		7:'account',
+		8:'description',
+		9:'allowance',
+		10:'drawing...it is not good so you take his word for it.',
+		11:'cupboard',
+		12:'hoard',
+		13:'stack',
+		14:'throng',
+		15:'bundle',
+		16:'set',
+		17:'store',
+		18:'assortment'	
 	}
-	return str(lootAdj[lootD1] + ' ' + lootName[lootD] + ' ' + lootType[lootD1])
+	return str(lootAdj[lootD] + ' ' + lootName[lootD1] + ' ' + lootType[lootD1])
 	
 def getLoot():
 	lootMult = 1
